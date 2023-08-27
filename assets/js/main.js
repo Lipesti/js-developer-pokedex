0b74ca5a29ed377c73fc7445ca835e67df5cd768
Felipe Santos <lipes.santos02@gmail.com>
2023-08-27 18:53:28 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,41 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMore = document.getElementById('loadMore')
-const limit = 10
-let offset = 0
+const pokemonList = document.getElementById('pokemonList');
+const loadMore = document.getElementById('loadMore');
+const limit = 10;
+let offset = 0;
 
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon, index) => `
+      <li class="pokemon ${pokemon.type}">
+        <span class="number">#00${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
 
+        <div class="detail">
+          <ol class="types">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+          </ol>
 
-function loadPokemonItens(offset,  limit){
-    
-    pokeApi.getPokemons(offset,  limit).then((pokemons = [] ) => {
-        const newHtml = pokemons.map((pokemon) => `
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#00${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+          <img src="${pokemon.photo}" alt="${pokemon.name}">
+        </div>
+        <button class="bDetails" type="button">Detalhes</button>
+      </li>`);
 
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
+    pokemonList.innerHTML += newHtml.join('');
 
-                    <img src="${pokemon.photo}"
-                        alt="${pokemon.name}">
-                </div>
-            </li>`
-            ).join('')
-        pokemonList.innerHTML += newHtml
-
-        const pokemonItems = document.querySelectorAll('.pokemon');
-            pokemonItems.forEach((item) => {
-        item.addEventListener('click', () => {
-          showPokemonDetails(item);
-        });
+    const bDetailsButtons = document.querySelectorAll('.bDetails');
+    bDetailsButtons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        const detailDiv = document.createElement('div');
+        const pokemon = pokemons[index]; 
+        detailDiv.innerHTML = `
+          <h3>${pokemon.name}</h3>
+          <p>Number: #00${pokemon.number}</p>
+          <p>Types: ${pokemon.types.join(', ')}</p>
+        `;
+        button.parentNode.appendChild(detailDiv);
       });
     });
+  });
 }
-    
-loadPokemonItens(offset,  limit)
 
-
-loadMore.addEventListener('click', () => {
-    offset += limit
-    loadPokemonItens(offset,  limit)
-})
-    
-
-
-
-  
-  
-   
+loadPokemonItems(offset, limit);
